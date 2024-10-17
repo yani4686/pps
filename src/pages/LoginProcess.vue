41cfa79a6ce7f3ece5a90cf50487ea7ec6440689
aa <bhgak@unisza.edu.my>
2024-10-17 08:47:17 +0800

    update login sso, axios and pinia

---
+++ src/pages/LoginProcess.vue
@@ -0,0 +1,27 @@
+<template>
+  <span> Check SSO... </span>
+</template>
+<script>
+import { useQuasar } from "quasar";
+import { api } from "src/boot/axios";
+import { useRouter, useRoute } from "vue-router";
+
+export default {
+  setup() {
+    var $q = useQuasar();
+    var route = useRoute();
+    var router = useRouter();
+    var token = route.params.token;
+
+    if (token == "") window.location.href = "https://portal.unisza.edu.my/";
+    else getToken(token);
+
+    function getToken(data) {
+      //session storage
+      $q.sessionStorage.set("token", data);
+      return router.push("/dashboard");
+    }
+    return {};
+  },
+};
+</script>
