e00c7ba762ab8fdd51abff07b3a6f5f39cddb766
NUR AHMARUL WARDAH ELIAS <ahmarulwardah@unisza.edu.my>
2024-10-28 16:22:40 +0800

    cHANGE DESIGN FOR LANDINGPAGE3

--- src/pages/LandingPage3.vue
+++ src/pages/LandingPage3.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="q-pa-none q-ml-xl" style="width: 90%; height: 60%">
+  <div class="q-pa-none q-mx-xl" style="width: 90%; height: 60%">
     <q-carousel animated v-model="slide" arrows navigation infinite>
       <q-carousel-slide
         :name="1"
@@ -16,18 +16,14 @@
         img-src="/images/bulatan_ilmu.png"
         style="width: 100%; height: 60%; object-fit: cover"
       />
-      <!-- <q-carousel-slide
-        :name="4"
-        img-src="https://cdn.quasar.dev/img/quasar.jpg"
-      /> -->
     </q-carousel>
   </div>
-
+  <!----------------------------------------------------------------------------------------------------------------------->
   <div class="q-pa-sm">
     <!-- Cards Container -->
-    <div class="row justify-center q-pa-lg q-ma-lg">
-      <q-card class="feature-card col-12 col-sm-3 orange-card">
-        <q-card-section class="card-content">
+    <div class="row card-layout q-pa-lg q-ma-lg">
+      <q-card class="q-mr-lg" style="width: 310px">
+        <q-card-section>
           <q-icon name="library_add" size="lg" class="icon-left" />
           <div class="card-text">
             <div class="text-subtitle2">New Programmes</div>
@@ -35,102 +31,153 @@
           </div>
         </q-card-section>
       </q-card>
-
-      <q-card class="feature-card col-12 col-sm-3 purple-card">
-        <q-card-section class="card-content">
-          <q-icon name="assignment_turned_in" size="lg" class="icon-left" />
+      <q-card class="q-mr-lg" style="width: 310px">
+        <q-card-section>
+          <q-icon name="library_add" size="lg" class="icon-left" />
           <div class="card-text">
-            <div class="text-subtitle2">Programmes approved by Faculty</div>
-            <div class="text-h5">294</div>
+            <div class="text-subtitle2">New Programmes</div>
+            <div class="text-h5">100</div>
           </div>
         </q-card-section>
       </q-card>
-
-      <q-card class="feature-card col-12 col-sm-3 blue-card">
-        <q-card-section class="card-content">
-          <q-icon name="add_task" size="lg" class="icon-left" />
+      <q-card class="q-mr-lg" style="width: 310px">
+        <q-card-section>
+          <q-icon name="library_add" size="lg" class="icon-left" />
           <div class="card-text">
-            <div class="text-subtitle2">Unverified Programmes</div>
-            <div class="text-h5">92%</div>
+            <div class="text-subtitle2">New Programmes</div>
+            <div class="text-h5">100</div>
           </div>
         </q-card-section>
       </q-card>
-
-      <q-card class="feature-card col-12 col-sm-3 green-card">
-        <q-card-section class="card-content">
-          <q-icon name="done_all" size="lg" class="icon-left" />
+      <q-card style="width: 300px">
+        <q-card-section>
+          <q-icon name="library_add" size="lg" class="icon-left" />
           <div class="card-text">
-            <div class="text-subtitle2">Approval Programmes</div>
-            <div class="text-h5">80%</div>
+            <div class="text-subtitle2">New Programmes</div>
+            <div class="text-h5">100</div>
           </div>
         </q-card-section>
       </q-card>
     </div>
+    <!-------------------------------------------------- Upcoming Activities ----------------------------------------------------->
+    <q-card-section>
+      <div class="row responsive-layout q-gutter-none">
+        <!-- First column: wider (8/12 of the width) -->
+        <div class="col-12 col-md-4">
+          <q-card style="height: 400px; width: 100%" class="q-mr-md">
+            <q-card-section>
+              <p style="font-size: large; color: black">Upcoming Activities</p>
+              <q-list bordered separator>
+                <q-item v-for="course in upcomingActivities" :key="course.id">
+                  <q-item-section>
+                    <div class="text-body text-bold ellipsis">
+                      {{ course.name }}
+                    </div>
+                    <div class="text-caption">
+                      {{ course.total }}
+                    </div>
+                  </q-item-section>
+                  <q-item-section side>
+                    <q-icon
+                      :name="showCourse ? 'visibility' : 'visibility_off'"
+                      class="cursor-pointer"
+                      size="xs"
+                      @click="toggleCourseVisibility"
+                    >
+                      <q-tooltip> View Activities </q-tooltip>
+                    </q-icon>
+                  </q-item-section>
+                </q-item>
+              </q-list>
+            </q-card-section>
+          </q-card>
+        </div>
 
-    <div class="row justify-center q-pa-lg q-ma-lg">
-      <!-- First Chart -->
-      <div class="col-12 col-md-6 q-mb-lg">
-        <q-card class="chart" flat bordered>
-          <chart
-            type="donut"
-            :options="donutChartOption"
-            :series="donutSeries"
-          />
-        </q-card>
+        <!------------------------------------------------- Todo List --------------------------------------------------------->
+        <div class="col-12 col-md-8">
+          <q-card style="width: 92%; height: 400px" class="q-mx-sm">
+            <q-card-section>
+              <div class="row todo-list q-mt-lg text-h5 text-center">
+                <b style="color: black">To Do List</b>
+              </div>
+              <q-table
+                :rows="tasks"
+                :columns="columns"
+                row-key="id"
+                flat
+                bordered
+                class="q-mt-md"
+              >
+                <template v-slot:body-cell-actions="props">
+                  <q-td align="center">
+                    <q-btn
+                      color="negative"
+                      label="DELETE"
+                      @click="deleteTask(props.row)"
+                      class="q-mr-sm"
+                    />
+                    <q-btn
+                      color="positive"
+                      label="FINISHED"
+                      @click="markFinished(props.row)"
+                    />
+                  </q-td>
+                </template>
+              </q-table>
+            </q-card-section>
+          </q-card>
+        </div>
       </div>
-
-      <!-- Second Chart -->
-      <div class="col-12 col-md-6 q-mb-lg">
-        <q-card class="chart" flat bordered>
-          <chart
-            type="donut"
-            :options="donutChartOption"
-            :series="donutSeries"
-          />
-        </q-card>
-      </div>
-    </div>
+    </q-card-section>
   </div>
 </template>
 
 <script>
 import { ref } from "vue";
-import chart from "vue3-apexcharts";
-
+import { useQuasar } from "quasar";
 export default {
-  components: {
-    chart,
-  },
-  data() {
-    const donutChartOption = ref({
-      chart: {
-        type: "donut",
-      },
-      labels: ["Diploma", "Degree", "Asasi", "Postgraduate"], // Categories
-      colors: ["#f78659", "#33FF57", "#f7599c", "#b659f7"], // Custom colors for each section
-      title: {
-        text: "Education Levels Distribution",
-      },
-      responsive: [
-        {
-          breakpoint: 600,
-          options: {
-            chart: {
-              width: 300,
-            },
-            legend: {
-              position: "bottom",
-            },
-          },
-        },
-      ],
-    });
+  setup() {
+    const $q = useQuasar();
 
-    const donutSeries = ref([30, 40, 15, 25]); // Values for Diploma, Degree, Asasi, Postgraduate
+    //----------------------------------------------- Upcoming Activities --------------------------------------
+    const upcomingActivities = ref([
+      {
+        id: 1,
+        name: "UI/ UX Design",
+        date: "Oct 20, 2024",
+        total: "30+ Courses",
+      },
+      { id: 2, name: "Web Dev", date: "Oct 25, 2024", total: "30+ Courses" },
+      {
+        id: 3,
+        name: "Mathematics",
+        date: "Oct 30, 2024",
+        total: "30+ Courses",
+      },
+    ]);
+    //------------------------------------- Tasks and Columns for To Do List ----------------------------------------
+    const tasks = ref([
+      { id: 1, todo: "Program Registration", status: "In progress" },
+      // { id: 2, todo: "Examination", status: "In progress" },
+      // { id: 3, todo: "New Student Intake", status: "In progress" },
+    ]);
+    const columns = ref([
+      { name: "id", label: "No.", field: "id", align: "left" },
+      { name: "todo", label: "Todo item", field: "todo", align: "left" },
+      { name: "status", label: "Status", field: "status", align: "left" },
+      { name: "actions", label: "Actions", align: "center" },
+    ]);
+    //------------------------------------------ function showCourse  --------------------------------------------------
+    function tooglePasswordVisibility() {
+      showCourse.value = !showCourse.value;
+    }
+
     return {
-      donutChartOption,
-      donutSeries,
       slide: ref(1),
+      upcomingActivities,
+      tasks,
+      columns,
+      tooglePasswordVisibility,
     };
   },
 };
@@ -168,6 +215,7 @@ export default {
 .icon-left {
   margin-right: 15px;
   flex-shrink: 0;
+  margin-left: 0;
 }
 
 /* Text Styling */
@@ -175,38 +223,53 @@ export default {
   text-align: left;
 }
 
-/* Left border color for each card */
-.purple-card {
-  border-left: 8px solid #ab9dd5;
-}
 .blue-card {
   border-left: 8px solid #6395ff;
+  width: 300px;
 }
 .green-card {
   border-left: 8px solid #99e76c;
+  width: 300px;
+}
+.purple-card {
+  border-left: 8px solid #f811f8;
+  width: 300px;
 }
 .orange-card {
-  border-left: 8px solid #e78e6c;
+  border-left: 8px solid #f4920a;
+  width: 300px;
 }
-
 /* Hover effects */
 .purple-card:hover {
-  background-color: rgba(171, 157, 213, 0.2); /* Light purple background */
+  background-color: rgba(171, 157, 213, 0.2);
 }
 
 .blue-card:hover {
-  background-color: rgba(99, 149, 255, 0.2); /* Light blue background */
+  background-color: rgba(99, 149, 255, 0.2);
 }
 
 .green-card:hover {
-  background-color: rgba(153, 231, 108, 0.2); /* Light green background */
+  background-color: rgba(153, 231, 108, 0.2);
 }
 
 .orange-card:hover {
   color: black;
-  background-color: rgba(222, 182, 173, 0.979); /* Light orange background */
+  background-color: rgba(222, 182, 173, 0.979);
 }
-
+.responsive-layout {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  margin-top: 0px;
+  margin-left: 20px;
+}
+.card-layout {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  margin-top: 50px;
+  margin-left: 20px;
+}
 /* Responsive adjustments */
 @media (max-width: 600px) {
   .cards-container {
